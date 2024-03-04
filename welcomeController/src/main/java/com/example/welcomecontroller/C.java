9341a5da094ab8afd4e245eb4b4a552086470ea6
AhmedAlharbi15 <159686025+AhmedAlharbi15@users.noreply.github.com>
2024-03-04 16:30:09 +0300

    Add files via upload

---
+++ welcomeController/src/main/java/com/example/welcomecontroller/C.java
@@ -0,0 +1,37 @@
+package com.example.welcomecontroller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class C {
+    @GetMapping("/name")
+    public String getName() {
+        return "My name is Ahmed";
+    }
+
+    @GetMapping("/age")
+    public String getAge()
+    {
+        return "My age is 23";
+    }
+
+    @GetMapping("/check/status")
+    public String getStatus()
+    {
+        return "Everything OK";
+    }
+
+    @GetMapping("/health")
+    public String getHealth()
+    {
+        return "Server health is up and running";
+    }
+
+    @GetMapping("/names")
+    public String[] getNames()
+    {
+        return new String[]{"Ahmed", "Ali", "Omar"};
+    }
+}
